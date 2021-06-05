be5721a4609a1a802aff5057222c88abfb274166
Ali <aliyildizoz909@gmail.com>
2021-06-05 13:44:51 +0300

    Added NorthwindContext

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+            //Veri tabanı zaten hazır olduğu için veritabanında herhangi bir işlem yapılması engellendi.
+            Database.SetInitializer<NorthwindContext>(null);
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
