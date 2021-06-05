02431ce465bb446f41064d7e2d1c2685cccec414
Ali <aliyildizoz909@gmail.com>
2021-06-05 13:28:59 +0300

    Added NhQueryableRepository

---
+++ DevFramework.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevFramework.Core.Entities;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+    public class NhQueryableRepository<T>: IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> _entities;
+
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table => this.Entites;
+
+        public virtual IQueryable<T> Entites
+        {
+            get { return _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>()); }
+        }
+    }
+}
