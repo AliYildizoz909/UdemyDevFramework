334b13ef2e25d69343994af3836d592d7bd0404f
Ali <aliyildizoz909@gmail.com>
2021-06-05 18:35:46 +0300

    Added SqlServerHelper and Tested ef

--- DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -9,7 +9,7 @@ namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
 {
     public class NorthwindContext : DbContext
     {
-        public NorthwindContext()
+        public NorthwindContext():base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
         {
             //Veri tabanı zaten hazır olduğu için veritabanında herhangi bir işlem yapılması engellendi.
             Database.SetInitializer<NorthwindContext>(null);
---
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using DevFramework.Core.DataAccess.NHibernate;
+using DevFramework.Northwind.DataAccess.Concrete.EntityFramework.Mappings;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers
+{
+    public class SqlServerHelper : NHibernateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            return Fluently.Configure().Database(MsSqlConfiguration.MsSql2012.ConnectionString(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")).Mappings(c => c.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())).BuildSessionFactory();
+        }
+    }
+}
