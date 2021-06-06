70df3e9032b5bd6ea083278b603cd2cce9366ef3
Ali <aliyildizoz909@gmail.com>
2021-06-06 12:15:05 +0300

    Added category op. for nh

---
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/Mappings/CategoryMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFramework.Northwind.Entities.Concrete;
+using FluentNHibernate.Mapping;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Mappings
+{
+    class CategoryMap : ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            Table(@"Categories");
+            //LazyLoad();
+            Id(p => p.CategoryId).Column("CategoryID");
+
+
+            Map(x => x.CategoryName).Column("CategoryName");
+        }
+    }
+}
---
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhCategoryDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFramework.Core.DataAccess.NHibernate;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhCategoryDal : NhEntityRepositoryBase<Category>, ICategoryDal
+    {
+        public NhCategoryDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
