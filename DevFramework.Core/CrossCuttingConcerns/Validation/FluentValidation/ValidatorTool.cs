f953e6fd2e4c63678626f429f3ea61e8e04a9fa8
Ali <aliyildizoz909@gmail.com>
2021-06-06 13:23:45 +0300

    Added ValidatorTool

---
+++ DevFramework.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class ValidatorTool
+    {
+        public static void FluentValidate<T>(IValidator validator, T entity) where T : class, new()
+        {
+            var result = validator.Validate(new ValidationContext<T>(entity));
+
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
