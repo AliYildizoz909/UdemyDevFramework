83e2e08ffb62d6d8335b35698aa54485d79e2d53
Ali <aliyildizoz909@gmail.com>
2021-06-08 14:34:44 +0300

    Added Loggers

---
+++ DevFramework.Core/CrossCuttingConcerns/Logging/Log4Net/Loggers/DatabaseLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers
+{
+    public class DatabaseLogger : LoggerService
+    {
+        public DatabaseLogger() : base(LogManager.GetLogger("DatabaseLogger"))
+        {
+        }
+    }
+}
---
+++ DevFramework.Core/CrossCuttingConcerns/Logging/Log4Net/Loggers/FileLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers
+{
+    public class FileLogger : LoggerService
+    {
+        public FileLogger() : base(LogManager.GetLogger("JsonFileLogger"))
+        {
+        }
+    }
+}
